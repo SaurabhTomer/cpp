5e9b9ea623ca5d4ae6f135c10ec26617c2f522d4
Saurabh Tomer <tomersaurabh91@gmail.com>
2025-08-04 00:04:21 +0530

    new

--- xor.cpp
+++ xor.cpp
@@ -1,18 +1,20 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-int find(vector<int> a){
+int find(vector<int> a)
+{
 
     int ans = 0;
 
-    for(int i =0 ; i < a.size() ; i++){ // this will xor 
-        ans = ans ^ a[i];
+    for (int i = 0; i < a.size(); i++)
+    {                     // this will xor all elements of array
+        ans = ans ^ a[i]; // xor of 0 with arr[i] will be arr[i]
     }
     return ans;
 }
-int main(){
-    vector <int> a = {4,1,2,1,2};
+int main()
+{
+    vector<int> a = {4, 1, 2, 1, 2};
 
-        int sol = find(a);
-        cout << "Missing element is :" << sol;
-    
+    int sol = find(a);
+    cout << "Missing element is :" << sol;
 }
